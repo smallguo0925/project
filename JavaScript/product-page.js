a9ff707298d7ef51c791d76d79cdc679049c3a7f
smallguo0925 <l09250701@gmail.com>
2023-12-16 22:39:22 +0800

    add productpage js

---
+++ JavaScript/product-page.js
@@ -0,0 +1,57 @@
+function $(id){
+    return document.getElementById(id);
+} 
+
+//+-盒子
+
+function init(){
+	//-------------------------------- 減
+	$("minus").onclick=function(){
+		let amount = $("numberbox").value;
+		if(amount>0){
+			$("numberbox").value = amount - 1;
+		}
+	}
+	//--------------------------------- 加
+	$("plus").onclick=function(){
+		let amount = parseInt($("numberbox").value);
+		$("numberbox").value = amount + 1;
+	}
+
+}	
+window.addEventListener("load", init, false);	
+
+//加入收藏
+function addfavorite(){
+    let heart = $('addfavorite');
+    if(heart.title === "加入收藏"){ //加入收藏
+        console.log(1)
+        $('addfavorite').src="../Images/red-heart.svg";
+        heart.title = "取消收藏";
+    } else {   //取消收藏     
+        heart.src="../Images/product-page-favorite.png";
+        heart.title = "加入收藏";
+    }
+
+}
+$('addfavorite').onclick=addfavorite;
+
+//點小圖秀大圖
+
+function showLarge(e){//e代表事件物件
+    let small=e.target; //event.target 屬性是永遠指向觸發事件的 DOM 物件
+    document.getElementById("large").src=small.src
+}
+    
+function select(){
+    let smalls=document.querySelectorAll(".smallimg img")
+    for(let i=0;i<smalls.length;i++){
+        smalls[i].onclick = showLarge
+    }
+}
+    
+document.getElementById("small").onclick = showLarge
+window.addEventListener("load", select, false);
+
+//大圖在手機板上輪播
+
