f75dd8a8d4cc5a98051e4bd20163c247e56491de
smallguo0925 <l09250701@gmail.com>
2023-12-16 16:05:43 +0800

    edit contactus js

--- JavaScript/contactus.js
+++ JavaScript/contactus.js
@@ -1,4 +1,4 @@
-//會員登入燈箱
+//contactus-會員登入燈箱
 function $(id){
     return document.getElementById(id);
 } 
@@ -32,6 +32,36 @@ function $(id){
             }
     });
     $("login").onclick=showLoginLightbox;
+//-----------------------------------------------
+//點我填寫聯絡表單
+    // 顯示燈箱
+    function showwrite() {
+        $("contactform").style.display = "block";
+        console.log("Clicked");
+    }
+    
+    // 隱藏燈箱
+    function closewrite() {
+        $("contactform").style.display = "none";
+    }
 
-        
+    window.addEventListener('click', function(e) {
+        var writeLightbox = $('contactform');
+        if (e.target === writeLightbox) {
+            closewrite();
+        }
+        var formclosebtn =$('closewrite');
+        if (e.target === formclosebtn) {
+            closewrite();
+            }
+    });
+    $("writeform").onclick=showwrite;
+
+//送出聯絡表單時的檢查
+$("write").onsubmit=function(){
+    alert(`已收到您的寶貴留言，樂食團隊會用最快速度處理您的問題。`)
+    closewrite();
+}
+
+
 
