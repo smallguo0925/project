02b6feef2ccf4d101bd9d9c7e3bb81df42066e74
smallguo0925 <l09250701@gmail.com>
2023-12-17 14:26:17 +0800

    edit product-pages-js

--- JavaScript/product-page.js
+++ JavaScript/product-page.js
@@ -1,21 +1,97 @@
-function $(id){
+function $id(id){
     return document.getElementById(id);
 } 
+
+//element-會員登入燈箱
+    // 顯示燈箱
+    function showLoginLightbox() {
+        $id("loginbox").style.display = "block";
+        console.log("Clicked");
+    }
+    
+    // 隱藏燈箱
+    function closeLoginLightbox() {
+        $id("loginbox").style.display = "none";
+    }
+    
+    // 提交登入表單的處理函式
+    function submitLoginForm() {
+        // 在這裡添加處理登入的邏輯
+        // 例如，檢查使用者名稱和密碼是否正確
+        // 如果正確，可以執行其他相關操作，然後關閉燈箱
+        // 如果不正確，可以顯示錯誤訊息
+        closeLoginLightbox();
+    }
+    window.addEventListener('click', function(e) {
+        var loginLightbox = $id('memlogin');
+        if (e.target === loginLightbox) {
+        closeLoginLightbox();
+        }
+        var formclosebtn =$id('formclose');
+        if (e.target === formclosebtn) {
+            closeLoginLightbox();
+            }
+    });
+    $id("login").onclick=showLoginLightbox;
+//會員登入驗證碼
+            // 生成驗證碼
+            let captcha = '';
+            function generateCaptcha() {
+                let canvas = $id('verify-code');
+                let context = canvas.getContext('2d');
+                let canvasWidth = canvas.width;
+                let canvasHeight = canvas.height;
+        
+                // 定義驗證碼允許的字符
+                let captchaText = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz123456789';
+                let captchaLength = 4; // 驗證碼長度
+                
+                // 清除Canvas
+                context.clearRect(0, 0, canvasWidth, canvasHeight);
+                // 在畫布上繪製驗證碼
+                for (let i = 0; i < captchaLength; i++) {
+                    let char = captchaText.charAt(Math.floor(Math.random() * captchaText.length));
+                    captcha += char;
+                    // 隨機顏色
+                    let colorR = Math.floor(Math.random() * (180 - 20)) + 20;
+                    let colorG = Math.floor(Math.random() * (180 - 20)) + 20;
+                    let colorB = Math.floor(Math.random() * (180 - 20)) + 20;
+                    // x, y 移動
+                    let x = 15 + i * 30;
+                    let y = 30 + Math.random() * 10;
+                    // 旋轉角度
+                    let angle = (Math.random() - 0.5) * 60 * Math.PI / 180; // 轉換為弧度
+                    context.save(); // 儲存當前繪圖狀態
+                    context.translate(x, y); // 將原點移動到文字位置
+                    context.rotate(angle); // 旋轉繪圖環境
+                    context.font = 'bold 30px Arial';
+                    context.fillStyle = `rgb(${colorR}, ${colorG}, ${colorB})`;
+                    context.fillText(char, 0, 0); // 在 (0, 0) 繪製文字
+                    context.restore(); // 恢復到原始繪圖狀態，避免旋轉影響其他元素
+                    x += 30; // 調整下一個字符的位置
+                }
+                return captcha;
+            }
+            generateCaptcha();
+            // 重新生成驗證碼
+            $id("refresh-code").onclick = generateCaptcha;
+        
+
 
 //+-盒子
 
 function init(){
 	//-------------------------------- 減
-	$("minus").onclick=function(){
-		let amount = $("numberbox").value;
+	$id("minus").onclick=function(){
+		let amount = $id("numberbox").value;
 		if(amount>0){
-			$("numberbox").value = amount - 1;
+			$id("numberbox").value = amount - 1;
 		}
 	}
 	//--------------------------------- 加
-	$("plus").onclick=function(){
-		let amount = parseInt($("numberbox").value);
-		$("numberbox").value = amount + 1;
+	$id("plus").onclick=function(){
+		let amount = parseInt($id("numberbox").value);
+		$id("numberbox").value = amount + 1;
 	}
 
 }	
@@ -23,10 +99,10 @@ window.addEventListener("load", init, false);
 
 //加入收藏
 function addfavorite(){
-    let heart = $('addfavorite');
+    let heart = $id('addfavorite');
     if(heart.title === "加入收藏"){ //加入收藏
         console.log(1)
-        $('addfavorite').src="../Images/red-heart.svg";
+        $id('addfavorite').src="../Images/red-heart.svg";
         heart.title = "取消收藏";
     } else {   //取消收藏     
         heart.src="../Images/product-page-favorite.png";
@@ -34,7 +110,7 @@ function addfavorite(){
     }
 
 }
-$('addfavorite').onclick=addfavorite;
+$id('addfavorite').onclick=addfavorite;
 
 //點小圖秀大圖(桌機)
 
@@ -70,3 +146,55 @@ function choice(){
 document.getElementById("small").onclick = showLarge
 window.addEventListener("load", choice, false);
 
+//其他推薦商品內容-手機板
+$(document).ready(function () {
+
+    let imgCount=$('#content li').length; //品項有幾項
+    console.log(imgCount)
+    let divwidth=$('#sliderBoard').width();//div的寬度
+    for(let i=0;i<imgCount;i++){
+        $('#contentButton').append(`<li></li>`)
+    }
+    $('#contentButton li:first').addClass('clicked')
+    // 針對Li的第一個小孩加上class="clicked"
+
+    $('#content li').width(divwidth)         //li的寬  (li的寬跟div一樣)
+    $('#content').width(divwidth * imgCount) //ui的寬  (li的寬 * 圖片個數) 
+
+    let index=0
+    let timer=setInterval(moveToNext,5000) //計時器，每五秒移動到下一個
+    //下面小圓圈圈被按的時候
+    $('#contentButton li').click(function(){
+        clearInterval(timer) //按下圓圈之後暫停季時器
+        index=$(this).index() //告訴我是按到第幾個按鈕(索引值)
+
+        $('#content').animate({
+            left:divwidth * index * -1,
+        })
+        $(this).addClass('clicked') //被按的那個加上.clicked
+        $('#contentButton li').not(this).removeClass ('clicked') //其他的除了你以外移除.clicked
+        timer=setInterval(moveToNext,5000)//計時器重新計算
+    })
+
+    //每五秒往左邊移動->
+    //計時器 setInterval(函數,毫秒)，若要暫停=
+    //let timer
+    //clearInterval(timer)
+
+    function moveToNext(){
+        //讓index不要一直加下去，所以要判斷他
+        if(index < imgCount-1){
+            index++
+        }else{
+            index=0
+        }
+
+        $('#content').animate({
+            left:divwidth * index * -1,
+        })
+        $(`#contentButton li:eq(${index})`).addClass('clicked')
+        $('#contentButton li').not(`:eq(${index})`).removeClass ('clicked') 
+
+    }
+
+});
